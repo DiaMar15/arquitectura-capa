c14f2e98c625f14fe5b3c957502202e6da09782c
EIMYR15 <loren.1518@hotmail.com>
2024-12-04 00:20:52 -0500

    SE implementa estructura de capas para frontend

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Route to render the index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/views/index.html');
+});
+
 //Add api routes
 const apiLibros = require('./routes/api')
 app.use('/libreria', apiLibros)
